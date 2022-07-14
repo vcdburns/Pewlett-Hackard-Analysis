b169ae380bc5e8684f3b21afa96d217efe165b45
Devin <vcdburns@gmail.com>
2022-07-14 18:43:51 -0400

    deliverable 2

--- Analysis Projects/Pewlett-Harckard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis Projects/Pewlett-Harckard-Analysis/Queries/Employee_Database_challenge.sql
@@ -31,3 +31,23 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY unique_titles.title
 ORDER BY unique_titles.count DESC;
+
+-- Deliverable 2
+
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no, 
+employees.first_name, 
+employees.last_name, 
+employees.birth_date,
+dept_emp.from_date, 
+dept_emp.to_date,
+titles.title
+
+INTO mentorship_eligibility
+
+FROM employees
+	JOIN dept_emp
+		ON (employees.emp_no = dept_emp.emp_no)
+	JOIN titles	
+		ON (employees.emp_no = titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01') AND
+		(employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
